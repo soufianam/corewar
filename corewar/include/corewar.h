f52d67f0900d15b571fa37ff07ceecb60928da91
Baptiste LEFEUVRE <cuikib@hotmail.fr>
2018-03-20 12:45:58 +0100

    corewar.h modif

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -6,7 +6,7 @@
 /*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 16:49:39 by blefeuvr          #+#    #+#             */
-/*   Updated: 2018/03/20 11:41:28 by blefeuvr         ###   ########.fr       */
+/*   Updated: 2018/03/20 12:45:42 by blefeuvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,30 +25,36 @@
 # define ND(x, y) N(x); S(" - "); N(y); L("")
 // ---------- //
 
-typedef struct			s_champion
+typedef struct		s_champion
 {
-	int					number;
-	char				*name;
-	int					*bin;
-}						t_champion;
+	int				number;
+	char			*name;
+	int				*bin;
+}					t_champion;
 
-typedef struct			s_process
+typedef struct		s_process
 {
-	char				registries[REG_NUMBER][REG_SIZE]; //r1 = player id
-	int					pc; //entrypoint du processus
-	int					carry; //init a 0
-	int					next_cycle; //init a 0
-	struct s_process	*next;
-}						t_process;
+	char			registries[REG_NUMBER][REG_SIZE]; //r1 = player id
+	int				pc; //entrypoint du processus
+	int				carry; //init a 0
+	int				next_cycle; //init a 0
+}					t_process;
 
-typedef struct			s_setting
+typedef struct		s_setting
 {
-	int					nbr_cycle;
-	int					nbr_champion;
-	t_champion			champion_tab[MAX_PLAYERS];
-	t_process			*process; //all process list
-}						t_setting;
+	int				nbr_cycle;
+	int				nbr_champion;
+	t_champion		champion_tab[MAX_PLAYERS];
+}					t_setting;
 
-int						cw_strisdig(char *str); // a supprimer ?
+typedef struct		s_vm
+{
+	t_setting		settings;
+	int				cycle;
+	t_list			*process; //all process list
+	int				*vm;
+}					t_vm;
+
+int					cw_strisdig(char *str); // a supprimer ?
 
 #endif
