c0609124a3ebbbe29a4a285aee00a95ad8b3a6f9
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-03-20 11:42:19 +0100

    Ajout d'une structure d'écrivant l'op_tab au saint du fichier op.c (temporaire)

--- corewar/src/op.c
+++ corewar/src/op.c
@@ -6,7 +6,7 @@
 /*   By: pprikazs <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/20 10:53:07 by pprikazs          #+#    #+#             */
-/*   Updated: 2018/03/20 10:55:43 by pprikazs         ###   ########.fr       */
+/*   Updated: 2018/03/20 11:27:31 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 typedef struct		s_op
 {
-	char			*cmd_name;
-	//...
-	//...
-	//...
+	char			*cmd_name; //Non de l'instruction
+	int				nb_registre_index;  // Nombre de registre + index == "equivaux au maximmum;
+	int				stuct_registre_schem; // Decription des structures acceptables.
+	int				opcode; //Opcode de l'option == index de l'optab
+	int				nb_cycle; //nombre de cycle qu'une operation met pour s'executé
+	char			*description; //Descriptif...
+	t_bool			octet_de_codage; //Indique si il y a un octet de codage
+	t_bool			cary_state; // SUPPOSITION: valeur du carry pour l'instruction (operateur logique pour y répondre???)...
 }					t_op;
 
 t_op    op_tab[17] =
