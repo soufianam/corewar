e0fadc3eac2d892b0b08c1516b44aa1fbfa39235
Baptiste LEFEUVRE <cuikib@hotmail.fr>
2018-03-20 09:25:43 +0100

    minor changes

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -6,7 +6,7 @@
 /*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 16:49:39 by blefeuvr          #+#    #+#             */
-/*   Updated: 2018/03/19 18:34:44 by cmaxime          ###   ########.fr       */
+/*   Updated: 2018/03/20 09:16:33 by blefeuvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,21 @@
 
 # include "libft.h"
 # include "op.h"
+# include "struct_corewar.h"
+
+// A commenter //
+# define L(x) ft_putendl_fd(x, 1)
+# define S(x) ft_putstr_fd(x, 1)
+# define N(x) ft_putnbr_fd(x, 1)
+# define C(x) ft_putchar_fd(x, 1)
+# define ND(x, y) N(x); S(" - "); N(y); L("")
+// ---------- //
 
 typedef struct	s_champion
 {
-	int		number;
-	char	*name;
-	int		*bin;
+	int			number;
+	char		*name;
+	int			*bin;
 }				t_champion;
 
 typedef struct	s_setting
@@ -29,6 +38,13 @@ typedef struct	s_setting
 	int			nbr_champion;
 	t_champion	champion_tab[MAX_PLAYERS];
 }				t_setting;
+
+typedef struct	s_processus
+{
+	char		registries[REG_NUMBER][REG_SIZE];
+	int			pc;
+	int			carry;
+}				t_processus;
 
 int				cw_strisdig(char *str);
 
