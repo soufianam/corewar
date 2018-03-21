e9c19545690c798512d295ae86def97040b4d6a9
Baptiste LEFEUVRE <cuikib@hotmail.fr>
2018-03-21 09:52:00 +0100

    add lstdelif to libft

---
+++ lib/libft/src/list/ft_lstdelif.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelif.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/21 09:45:33 by blefeuvr          #+#    #+#             */
+/*   Updated: 2018/03/21 09:51:24 by blefeuvr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelif(t_list **begin_list, void *content_ref, int (*cmp)())
+{
+	t_list	*head;
+	t_list	*prev;
+
+	prev = 0;
+	if (!begin_list)
+		return ;
+	head = *begin_list;
+	while (head != 0)
+	{
+		if (cmp(head->content, content_ref) == 0)
+		{
+			if (prev == 0)
+				*begin_list = head->next;
+			else
+				prev->next = head->next;
+			free(head);
+		}
+		prev = head;
+		head = head->next;
+	}
+}
