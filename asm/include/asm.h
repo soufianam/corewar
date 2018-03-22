25c1422a070f36b2badd189e480a93eb053633de
Paul PRIKAZSKY <paul.prikazsky@gmail.com>
2018-03-22 18:48:39 +0100

    Ajout de la global op_tab et de sa structure

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -6,15 +6,34 @@
 /*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 17:09:49 by blefeuvr          #+#    #+#             */
-/*   Updated: 2018/03/21 18:18:28 by cmaxime          ###   ########.fr       */
+/*   Updated: 2018/03/22 18:45:57 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASM_H
 # define ASM_H
 
+# include <sys/cdefs.h>
 # include "libft.h"
 # include "op.h"
+
+typedef enum	e_bool
+{
+	FALSE = 0,
+	TRUE = 0
+}				t_bool;
+
+typedef struct	s_op
+{
+	char		*ins;
+	int8_t		nb_param;
+	int8_t		param[4];
+	int8_t		opcode;
+	int			nb_cycle;
+	char		*desc;
+	t_bool		carry;
+	t_bool		codage;
+}				t_op;
 
 typedef struct	s_param
 {
--- asm/src/main.c
+++ asm/src/main.c
@@ -6,8 +6,20 @@
 /*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 16:56:30 by blefeuvr          #+#    #+#             */
-/*   Updated: 2018/03/19 16:56:31 by blefeuvr         ###   ########.fr       */
+/*   Updated: 2018/03/22 18:44:53 by pprikazs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include "op.h"
+#include "asm.h"
+#include "global.c"
 
+int				main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	ft_putstr(op_tab[0].ins);
+	ft_putnbr(op_tab[0].nb_param);
+	return (0);
+}
