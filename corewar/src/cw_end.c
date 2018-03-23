98030f43b8b125f6ab9e84b894474533935e5888
Baptiste LEFEUVRE <cuikib@hotmail.fr>
2018-03-23 16:34:44 +0100

    changed behavior when no live made

--- corewar/src/cw_end.c
+++ corewar/src/cw_end.c
@@ -6,7 +6,7 @@
 /*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/22 11:21:13 by blefeuvr          #+#    #+#             */
-/*   Updated: 2018/03/23 16:21:48 by blefeuvr         ###   ########.fr       */
+/*   Updated: 2018/03/23 16:34:01 by blefeuvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,19 @@ static char	*cw_find_player(t_setting setting, int index)
 			return (setting.champion_tab[i].name);
 		i--;
 	}
-	cw_error(ERR_UNKNOW);
 	return (0);
 }
 
 void	cw_game_over(t_vm *vm)
 {
 	int		index;
+	char	*player;
 
 	index = vm->loop.last_live.index;
-	if (index == -1)
-		ft_printf("No procces found living");
+	player = cw_find_player(vm->setting, index);
+	if (index == -1 || !player)
+		ft_printf("No process found living for a champion\n");
 	else
-		ft_printf("Player %d %s won\n", index, \
-			cw_find_player(vm->setting, index));
+		ft_printf("Player %d %s won\n", index, player);
 	exit(0);
 }
