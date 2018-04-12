2c03c56c06f29672d95205ec11eb5585b17099ad
cuikib <cuikib@hotmail.fr>
2018-04-12 21:57:52 +0200

    corrected memjoin to make printf works

--- lib/libft/src/mem/ft_memjoin.c
+++ lib/libft/src/mem/ft_memjoin.c
@@ -22,9 +22,5 @@ char	*ft_memjoin(char *s1, char *s2, size_t l1, size_t l2)
 		return (NULL);
 	ft_memcpy(dst, s1, l1);
 	ft_memcpy(dst + l1, s2, l2);
-	if (s1)
-		ft_strdel(&s1);
-	if (s2)
-		ft_strdel(&s2);
 	return (dst);
 }
