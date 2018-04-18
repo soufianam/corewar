354c112eef1b588f214531b114546f6c46e7f3f1
Baptiste LEFEUVRE <cuikib@hotmail.fr>
2018-04-18 16:21:08 +0200

    corrected st - add

--- corewar/src/instructions/cw_add.c
+++ corewar/src/instructions/cw_add.c
@@ -6,7 +6,7 @@
 /*   By: tdeborde <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/30 16:48:08 by tdeborde          #+#    #+#             */
-/*   Updated: 2018/04/18 12:55:34 by tdeborde         ###   ########.fr       */
+/*   Updated: 2018/04/18 16:18:53 by blefeuvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,10 @@ int		cw_add(t_vm *vm, t_process *process)
 	}
 	process->pc = (process->pc + 1) % MEM_SIZE;
 	cw_wait_process(vm, process);
+	if (DEBUG)
+	{
+		ft_printf("--cycle %d--\nadd: r%d (%d) + r%d (%d) -> r%d\n",\
+				vm->cycle, param[0], cw_get_4(process->registries[param[0] - 1]), param[1], cw_get_4(process->registries[param[1] - 1]), param[2]);
+	}
 	return (1);
 }
