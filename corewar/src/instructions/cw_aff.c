125711aea0c9e4b43f39b3242c3dcb6802c0e7b7
Tristan DEBORDE <tr.deborde@gmail.com>
2018-04-18 12:59:31 +0200

    aff now taking register + debugging registries

--- corewar/src/instructions/cw_aff.c
+++ corewar/src/instructions/cw_aff.c
@@ -6,7 +6,7 @@
 /*   By: tdeborde <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/31 14:10:34 by tdeborde          #+#    #+#             */
-/*   Updated: 2018/04/18 12:27:33 by blefeuvr         ###   ########.fr       */
+/*   Updated: 2018/04/18 12:58:36 by tdeborde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int			cw_aff(t_vm *vm, t_process *process)
 {
-	int	param;
+	int		param;
 
-	param = cw_get_2(&(vm->vm[(process->pc + process->entrypoint + 2) % MEM_SIZE]));
-	param = param % 256;
+	param = cw_get_1(&(vm->vm[(process->pc + process->entrypoint + 2) % MEM_SIZE]));
+	param = cw_get_4(process->registries[param - 1]) % 256;
 	process->pc = (process->pc + 4) % MEM_SIZE;
-	ft_printf("%C", (unsigned char)param);
+	ft_printf("%C\n", (unsigned char)param);
 	if (DEBUG)
 	{
 		ft_printf("--cycle %d--\naff: %d", vm->cycle, param);
