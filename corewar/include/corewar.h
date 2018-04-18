ed70bcf62da148f31d4c39351f3f3df2ac4c2246
Tristan DEBORDE <tr.deborde@gmail.com>
2018-04-18 19:59:04 +0200

    changed char into unsigned char

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -6,7 +6,7 @@
 /*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 18:20:12 by blefeuvr          #+#    #+#             */
-/*   Updated: 2018/04/18 12:34:41 by tdeborde         ###   ########.fr       */
+/*   Updated: 2018/04/18 19:58:45 by tdeborde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ typedef struct		s_champion
 
 typedef struct		s_process
 {
-	char			registries[REG_NUMBER][REG_SIZE]; //r1 = player id
+	unsigned char	registries[REG_NUMBER][REG_SIZE]; //r1 = player id
 	int				entrypoint; //entrypoint
 	int				pc; //init a 0
 	int				carry; //init a 0
@@ -75,7 +75,7 @@ typedef struct		s_vm
 	t_setting		setting;
 	int				cycle;
 	t_list			*process; //all process list
-	char			*vm;
+	unsigned char	*vm;
 	t_loop			loop;
 }					t_vm;
 
@@ -149,11 +149,11 @@ int			cw_lld(t_vm *vm, t_process *process);
 int			cw_lldi(t_vm *vm, t_process *process);
 int			cw_lfork(t_vm *vm, t_process *process);
 int			cw_aff(t_vm *vm, t_process *process);
-int			cw_get_1(char *vm);
-int			cw_get_2(char *vm);
-int			cw_get_4(char *vm);
+int			cw_get_1(unsigned char *vm);
+int			cw_get_2(unsigned char *vm);
+int			cw_get_4(unsigned char *vm);
 int			cw_check_reg(int param);
-void		cw_rev_get(char *registries, int nb);
+void		cw_rev_get(unsigned char *registries, int nb);
 int			cw_read_ocp(t_vm *vm, t_process *process, int *param,
 		unsigned char ocp_trunc);
 int			cw_read_ocp_short(t_vm *vm, t_process *process, int *param,
