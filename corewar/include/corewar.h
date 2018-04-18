f7a19d5bcd4650eefbddb49f445a7f4cff6dd6a6
Tristan DEBORDE <tr.deborde@gmail.com>
2018-04-18 12:35:15 +0200

    included all funcs in corewar.h

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -6,7 +6,7 @@
 /*   By: blefeuvr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 18:20:12 by blefeuvr          #+#    #+#             */
-/*   Updated: 2018/04/18 12:01:00 by blefeuvr         ###   ########.fr       */
+/*   Updated: 2018/04/18 12:34:41 by tdeborde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,6 +140,15 @@ int			cw_sti(t_vm *vm, t_process *process);
 int			cw_and(t_vm *vm, t_process *process);
 int			cw_zjump(t_vm *vm, t_process *process);
 int			cw_add(t_vm *vm, t_process *process);
+int			cw_sub(t_vm *vm, t_process *process);
+int			cw_or(t_vm *vm, t_process *process);
+int			cw_xor(t_vm *vm, t_process *process);
+int			cw_ldi(t_vm *vm, t_process *process);
+int			cw_fork(t_vm *vm, t_process *process);
+int			cw_lld(t_vm *vm, t_process *process);
+int			cw_lldi(t_vm *vm, t_process *process);
+int			cw_lfork(t_vm *vm, t_process *process);
+int			cw_aff(t_vm *vm, t_process *process);
 int			cw_get_1(char *vm);
 int			cw_get_2(char *vm);
 int			cw_get_4(char *vm);
