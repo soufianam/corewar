614d87e788f52d699954c57cea296a1659a09bbc
Tristan DEBORDE <tr.deborde@gmail.com>
2018-04-19 16:49:06 +0200

    debugging offset in ldi lldi

--- corewar/src/instructions/cw_ldi.c
+++ corewar/src/instructions/cw_ldi.c
@@ -6,7 +6,7 @@
 /*   By: tdeborde <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 17:12:39 by tdeborde          #+#    #+#             */
-/*   Updated: 2018/04/19 15:02:33 by tdeborde         ###   ########.fr       */
+/*   Updated: 2018/04/19 16:47:47 by tdeborde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,23 @@ int		cw_ldi_param(t_vm *vm, t_process *process, int param[2], int ret[2])
 		else if (ret[i] == 2)
 			param[i] = cw_get_4(&(vm->vm[(process->pc + process->entrypoint
 							- offset + ((short)param[i] % IDX_MOD)) % MEM_SIZE]));
+		ret[i] = ret[i] == 4 ? 2 : ret[i];
 		ocp = ocp << 2;
 	}
-	return (check);
+	return (check ? offset : 0);
 }
 
 int		cw_ldi(t_vm *vm, t_process *process)
 {
 	int				param[3];
 	int				ret[3];
+	int				offset;
 
 	process->pc = (process->pc + 1) % MEM_SIZE;
-	if ((cw_ldi_param(vm, process, param, ret)))
+	if ((offset = cw_ldi_param(vm, process, param, ret)))
 	{
 		ft_memcpy(process->registries[param[2] - 1], &(vm->vm[(process->pc
-			+ process->entrypoint + param[0] + param[1] - ret[0] - 4) % MEM_SIZE]), REG_SIZE);
+			+ process->entrypoint + param[0] + param[1] - offset) % MEM_SIZE]), REG_SIZE);
 		process->carry = !param[0] ? 1 : 0;
 	}
 	process->pc = (process->pc + 1) % MEM_SIZE;
