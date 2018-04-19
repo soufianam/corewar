38ee9b1c039a6bb04b86965c806b827c16f86abb
Baptiste LEFEUVRE <cuikib@hotmail.fr>
2018-04-19 11:11:56 +0200

    correct width of indirect ld

--- corewar/src/instructions/cw_ld.c
+++ corewar/src/instructions/cw_ld.c
@@ -6,7 +6,7 @@
 /*   By: tdeborde <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 17:12:39 by tdeborde          #+#    #+#             */
-/*   Updated: 2018/04/18 19:04:27 by tdeborde         ###   ########.fr       */
+/*   Updated: 2018/04/19 11:11:35 by blefeuvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,15 @@ int		cw_ld_param(t_vm *vm, t_process *process, int param[2], int ret[2])
 			ret[i] += 1;
 			check = 0;
 		}
-		else if (ret[i] == 2)
-			param[i] = cw_get_2(&(vm->vm[(process->pc + process->entrypoint
-							- offset + (param[i] % 512)) % MEM_SIZE]));
-		ocp = ocp << 2;
 		offset += ret[i];
+		if (ret[i] == 2)
+		{
+			ft_printf("%d\n", (process->pc + process->entrypoint - offset + (param[i] % 512)) % MEM_SIZE);
+			param[i] = cw_get_4(&(vm->vm[(process->pc + process->entrypoint
+							- offset + (param[i] % 512)) % MEM_SIZE]));
+			ft_printf("%d\n", param[i]);
+		}
+		ocp = ocp << 2;
 	}
 	return (check);
 }
